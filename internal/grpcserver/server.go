98447be0561fb4f529f6383a96c3c9da5a63222c
allliance <alians310322@gmail.com>
2021-10-24 02:37:25 +0330

    Prevent fetching same results multiple time from python grpc server

--- internal/grpcserver/server.go
+++ internal/grpcserver/server.go
@@ -6,6 +6,7 @@ import (
 	client "microservice-grpc/internal/client"
 	appdetail "microservice-grpc/pkg/appdetail"
 	appcache "microservice-grpc/pkg/cache"
+	"sync"
 
 	proto "github.com/golang/protobuf/proto"
 	"google.golang.org/grpc"
@@ -17,25 +18,52 @@ type cacheServer struct {
 	clientInstance appdetail.AppDetailClient
 }
 
+type pendingRequest struct {
+	packageName string
+	mu          *sync.Mutex
+}
+
+var pendingRequests map[string]*pendingRequest = make(map[string]*pendingRequest)
+
 func (server cacheServer) GetAppDetail(ctx context.Context, request *appdetail.GetAppDetailRequest) (*appdetail.GetAppDetailReply, error) {
-	if data, err := server.cache.Retrieve(getCacheName(request.GetPackageName())); err != nil {
-		result := &appdetail.GetAppDetailReply{}
-		err := proto.Unmarshal(data, result)
-		if err != nil {
-			return result, err
-		}
+	cacheName := getCacheName(request.PackageName)
+	result, err := checkForDataInCache(server.cache, cacheName)
+	if err == nil {
+		return result, nil
+	}
+	pending := pendingRequests[request.GetPackageName()]
+	if pending == nil {
+		pending = &pendingRequest{packageName: cacheName, mu: &sync.Mutex{}}
+		defer delete(pendingRequests, cacheName)
+	}
+	pending.mu.Lock()
+	defer pending.mu.Unlock()
+	if result, err := checkForDataInCache(server.cache, cacheName); err == nil {
 		return result, nil
 	}
 	appDetail, err := client.GetAppDetail(ctx, request.GetPackageName(), server.clientInstance)
 	if err != nil {
 		return nil, err
 	}
-	if data, err := proto.Marshal(appDetail); err != nil {
+	appDetailReply, err := proto.Marshal(appDetail)
+	if err != nil {
 		return nil, err
-	} else {
-		server.cache.CacheApp(getCacheName(request.GetPackageName()), data)
 	}
+	server.cache.CacheApp(getCacheName(request.GetPackageName()), appDetailReply)
 	return appDetail, nil
+}
+
+func checkForDataInCache(cache appcache.AppDetailCache, key string) (*appdetail.GetAppDetailReply, error) {
+	data, err := cache.Retrieve(key)
+	if err == nil {
+		result := &appdetail.GetAppDetailReply{}
+		err := proto.Unmarshal(data, result)
+		if err != nil {
+			return nil, err
+		}
+		return result, nil
+	}
+	return nil, err
 }
 
 func getCacheName(packageName string) string {
